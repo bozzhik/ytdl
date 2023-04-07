ab2f01e5925445d50407488502441047729534a1
max <bozzhik@ya.ru>
2023-04-07 16:07:34 +0300

    add the default functionality after clicking the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import {useCallback, useState} from 'react'
 
 export default function App() {
+  const [buttonColor, setButtonColor] = useState('')
+
+  const handleClick = useCallback(() => {
+    console.log('Button clicked!')
+    setButtonColor('#1095c1')
+    setTimeout(() => {
+      setButtonColor('')
+    }, 1500)
+  }, [])
+
   return (
     <div className="h-screen grid place-items-center">
       <div className="w-1/3 mx-auto">
-        <h2 className="text-3xl mt-7 mb-3">YouTube Downloader</h2>
+        <h2 className="text-3xl mb-4">YouTube Downloader</h2>
         <form>
+          <label htmlFor="url">Enter YouTube video URL:</label>
           <input type="text" id="url" name="url" placeholder="Enter YouTube video URL" />
-          <button type="submit" className="py-3 button">
+          <button id="button" type="submit" role="button" className="py-3 button" onClick={handleClick} style={{backgroundColor: buttonColor}}>
             Download
           </button>
         </form>
